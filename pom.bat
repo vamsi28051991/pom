6efed02f7864c2e33c7485042f9e22edb7737698
Vamsi <vamsi.thonduri@gmail.com>
2016-12-22 15:18:35 -0500

    batch file modified 2

--- pom.bat
+++ pom.bat
@@ -1,5 +1,5 @@
 set projectLocation=https://github.com/vamsi28051991/pom
 cd %projectLocation%
 set classpath=%projectLocation%/tree/master/bin;%projectLocation%/tree/master/Lib/*
-java org.testng.TestNG %projectLocation%\testng.xml
+java org.testng.TestNG %projectLocation%/blob/master/testng.xml
 pause
